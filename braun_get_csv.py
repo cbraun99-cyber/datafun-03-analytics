6ea7ecfd79e2e2e8d1a9ef3229d3070fea9f0e25
Chris Braun <cbraun99@gmail.com>
2025-09-08 12:02:53 -0500

    Updated link in braun_get_csv.py

--- braun_get_csv.py
+++ braun_get_csv.py
@@ -93,9 +93,9 @@ def main():
     """
     Main function to demonstrate fetching CSV data.
     """
-    csv_url = 'https://raw.githubusercontent.com/MainakRepositor/Datasets/master/World%20Happiness%20Data/2020.csv'
+    csv_url = 'https://www.fdic.gov/bank/individual/failed/banklist.csv'
     logger.info("Starting CSV fetch demonstration...")
-    fetch_csv_file(FETCHED_DATA_DIR, "2020_happiness.csv", csv_url)
+    fetch_csv_file(FETCHED_DATA_DIR, "failed_banklist.csv", csv_url)
 
 #####################################
 # Conditional Execution
